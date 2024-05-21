a39a636e00a35b084c6e64f74452a17d97372464
ChristofferFaugstad <christoffer.faugstad@cgi.com>
2024-05-21 17:28:30 +0200

    Refactor code

--- day 1/day1-2.py
+++ day 1/day1-2.py
@@ -4,54 +4,22 @@ Created on Mon Dec  4 18:43:19 2023
 
 @author: Siri
 """
-sum = 0
-
-digitsText = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9}
 
 def get_first_num(line):
     for character in line:
         if (character.isdigit()):
             return int(character)
-
-
-    # i = 0
-    # while i <= len(line):
-    #     digits = list(filter(str.isdigit, line[:i]))
-    #     if len(digits) > 0:
-    #         return digits[0]
-    #     for text, num in digitsText.items():
-    #         if (line[:i].find(text) != -1):
-    #             return num
-    #     i += 1
         
 def get_last_num(line):
     for character in line[::-1]:
         if (character.isdigit()):
             return int(character)
-    # i = len(line)-1
-    # while i >= 0:
-    #     digits = list(filter(str.isdigit, line[i:]))
-    #     if len(digits) > 0:
-    #         return digits[0]
-    #     for text, num in digitsText.items():
-    #         if (line[i:].find(text) != -1):
-    #             return num
-    #     i -= 1
     
 
 with open("day 1/input_day1_full.txt", "r") as file:
     lines = file.readlines()
     
+sum = 0
 for line in lines:
     firstNum = get_first_num(line)
     lastNum = get_last_num(line)
